4b995e3a30f6a88b668011e4c92c7da5f5e481a8
Samuel Carlsson <samuel.carlsson@gmail.com>
2024-02-27 08:22:04 +0100

    refactor: functions instead of lambdas

--- server.cpp
+++ server.cpp
@@ -1,18 +1,19 @@
 #include "httplib.h"
 
+void pass(const httplib::Request &, httplib::Response &res) {
+    res.set_content("PASSED", "text/plain");
+}
+
+void fail(const httplib::Request &, httplib::Response &res) {
+    res.set_content("FAILED", "text/plain");
+    res.status = 500;
+}
+
 int main() {
     httplib::Server server;
-
     server.set_mount_point("/", "./");
-    server.Get("/pass", [](const httplib::Request &, httplib::Response &res) {
-        res.set_content("PASSED", "text/plain");
-    });
-
-    server.Get("/fail", [](const httplib::Request &, httplib::Response &res) {
-        res.set_content("FAILED", "text/plain");
-        res.status = 500;
-    });
-
+    server.Get("/pass", pass);
+    server.Get("/fail", fail);
     server.listen("127.0.0.1", 8080);
     return 0;
 }
